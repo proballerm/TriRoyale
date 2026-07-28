7565cfc905e253d92aa9d040e9ee686d6e7299c3
Prabal Malavalli <prabal5587@gmail.com>
2026-07-28 16:23:20 -0700

    Register tournament spectator socket events

--- lib/registerTournamentSocketHandlers.ts
+++ lib/registerTournamentSocketHandlers.ts
@@ -1,4 +1,13 @@
-export {
+import type { Server, Socket } from "socket.io";
+import {
   getTournamentCoordinator,
-  registerTournamentSocketHandlers,
+  registerTournamentSocketHandlers as registerGameplayHandlers,
 } from "./tournamentSocketHandlers";
+import { registerTournamentSpectatorHandlers } from "./registerTournamentSpectatorHandlers";
+
+export { getTournamentCoordinator };
+
+export function registerTournamentSocketHandlers(io: Server, socket: Socket): void {
+  registerGameplayHandlers(io, socket);
+  registerTournamentSpectatorHandlers(io, socket);
+}
